9bf44f50ef0d27b2b53119b7d5166b4519db8e47
shruti2512 <shruti.paathak@gmail.com>
2023-07-06 20:09:37 -0500

    Add files via upload

---
+++ propertyzone.php
@@ -0,0 +1,42 @@
+<?php
+
+
+echo '<img src="https://propertyzone.info/projects/1/landing.jpg" height="550" width="1250">';
+echo "<h1> <font color=blue><center> PROVIDENT SUNWORTH CITY </center></font color></h1>";
+
+
+echo "<h3><font color=blue><center>Premium 2 & 3BHK Apartments On Mysore Road Bangalore - 
+<mark>Nearing Completion</mark></center></font color></p></h3>";
+?>
+<p style="border-width:3px; border-style:opaque; border-color:#FF0000; padding: 1em;">
+Provident Sunworth City offers <strong>2BHK & 3BHK Apartments(Ready To Move Soon) Off Mysore Road </strong> near Challaghatta Metro Station, Bangalore. It is a part of an <strong> integrated lifestyle township </strong> sprawling over <strong>~60 acres,</strong> a home at Provident Sunworth City brings every convenience to your doorstep without any compromise on connectivity to the rest of Bengaluru. The project offers signal free commute between Hosur and Tumkur road via NICE Road.
+Apartments at Provident Sunworth City are planned with minute attention to detail and functionality, and come with 50+ leisure amenities to meet your fitness & recreational choices at a thriving location with proximity to major education, healthcare and leisure avenues.
+At Provident, it is our endeavour to make each day in and out of your home, a joyful experience like nowhere else. Book a site visit to avail limited period special offers.
+<br><br>
+<strong>RERA NO. - PRM/KA/RERA/1251/310/PR/190529/002585</strong></p>
+
+
+//carousel of pictures
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/WilderBuildingSummerSolstice.jpg" height="550" width="1250" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.istockphoto.com/id/511061090/photo/business-office-building-in-london-england.jpg?b=1&s=612x612&w=0&k=20&c=HqM6ioLM2ynkcQXXLd3-thXTGKnBqbynW5SLhLfbHaU=" height="550" width="1250" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.themillsbuilding.com/userfiles/cms/building/images/1/building.jpg" height="550" width="1250" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
